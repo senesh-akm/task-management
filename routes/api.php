bd9c3a1da0f573429460c109eff2bd8c39a83b72
Ranusha Senesh Akmeemana <seneshak2001@gmail.com>
2024-07-19 20:25:02 +0530

    Added CRUD for this system.

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,18 @@
 <?php
 
+use App\Http\Controllers\TaskController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+// Define a route for creating a task (POST)
+Route::post('/tasks', [TaskController::class, 'store']);
+
+// Optionally, define other routes for tasks
+Route::get('/tasks', [TaskController::class, 'index']);
+Route::get('/tasks/{task}', [TaskController::class, 'show']);
+Route::put('/tasks/{task}', [TaskController::class, 'update']);
+Route::delete('/tasks/{task}', [TaskController::class, 'destroy']);
